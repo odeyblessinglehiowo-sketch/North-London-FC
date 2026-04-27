47f4013633dab8a741f1a8c6fce3befe4cb49e9f
Odey Blessing Lehiowo <odeyblessinglehiowo@gmail.com>
2026-04-27 18:44:57 +0100

    reg page

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,27 +1,28 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: Request) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+  if (!process.env.RESEND_API_KEY) {
+    return Response.json({ error: "Missing API key" });
+  }
+
   try {
     const body = await req.json();
 
     const { email, firstName } = body;
 
-    const data = await resend.emails.send({
+    await resend.emails.send({
       from: "North London FC <onboarding@resend.dev>",
       to: email,
       subject: "Registration Confirmed 🎉",
       html: `
-        <div style="font-family:sans-serif;">
-          <h2>You're in, ${firstName}! ⚽</h2>
-          <p>Your spot for the Arsenal Watch Party has been confirmed.</p>
-          <p>Get ready for an amazing experience 🔥</p>
-        </div>
+        <h2>You're in, ${firstName}! ⚽</h2>
+        <p>Your registration is confirmed.</p>
       `,
     });
 
-    return Response.json({ success: true, data });
+    return Response.json({ success: true });
   } catch (error) {
     console.log(error);
     return Response.json({ success: false });
